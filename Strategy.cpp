7f34b1d2c18ede0c4101681c3064409063975929
hma-3 <maria.gula0307@gmail.com>
2023-12-17 12:05:29 +0200

    Add project files.

---
+++ Strategy.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Strategy {
+public:
+    virtual void execute() = 0;
+};
+
+class ConcreteStrategyA : public Strategy {
+public:
+    void execute() override {
+        cout << "Executing Strategy A" << endl;
+    }
+};
+
+class ConcreteStrategyB : public Strategy {
+public:
+    void execute() override {
+        cout << "Executing Strategy B" << endl;
+    }
+};
+
+class Context {
+    Strategy* strategy;
+public:
+    void setStrategy(Strategy* strategy) {
+        this->strategy = strategy;
+    }
+
+    void executeStrategy() {
+        strategy->execute();
+    }
+};
+
+int main() {
+    Context context;
+
+    ConcreteStrategyA strategyA;
+    ConcreteStrategyB strategyB;
+
+    context.setStrategy(&strategyA);
+    context.executeStrategy();
+
+    context.setStrategy(&strategyB);
+    context.executeStrategy();
+
+    return 0;
+}
